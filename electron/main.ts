d605fa197d7808e299ddb61b813d1a6422f48747
Tyler Reynolds <me@reynolds.tj>
2022-04-16 19:39:01 -0700

    Set window size constraints

--- electron/main.ts
+++ electron/main.ts
@@ -21,8 +21,11 @@ function createWindow() {
     Store.initRenderer();
     mainWindow = new BrowserWindow({
         // icon: path.join(assetsPath, 'assets', 'icon.png'),
-        width: 1100,
-        height: 700,
+        width: 900,
+        height: 1200,
+        minWidth: 500,
+        maxWidth: 1200,
+        minHeight: 150,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
